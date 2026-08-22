872d3c0c22bf06659e0f7e0f445c9b81a36f6301
cristiansuarezgonzalez <44825568+cristiansuarezgonzalez@users.noreply.github.com>
2026-08-22 12:21:56 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// El base debe coincidir EXACTAMENTE (mayúsculas incluidas) con el nombre
-// del repositorio de GitHub: https://cristiansuarezgonzalez.github.io/Carga-Deepal/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Carga-Deepal/'
-});
